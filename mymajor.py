c6add08f747cccff07fdd3955c4b31072a70c4fb
JoyH127 <97911806+JoyH127@users.noreply.github.com>
2022-03-02 23:52:25 -0500

    Delete mymajor.py

--- mymajor.py
+++
@@ -1,6 +0,0 @@
-# Promt the student for their major.
-# Then display the major to the user with a helpful message
-
-Mymajor = input("What is your major?\n")
-print("Your major is:")
-print(Mymajor)
